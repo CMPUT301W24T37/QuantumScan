e33e1fc3fda883e6e12a25cab18dc39bf5632dab
austin-16 <austin727303178@gmail.com>
2024-02-29 19:08:51 -0700

    Update Event, Organizer and Attendee. Create User.

--- app/app/src/main/java/com/example/quantumscan/Organizer.java
+++ app/app/src/main/java/com/example/quantumscan/Organizer.java
@@ -1,4 +1,38 @@
 package com.example.quantumscan;
 
 public class Organizer {
+    private final User user;
+    private final Event event;
+
+    // Constructors
+
+    public Organizer(User user, Event event) {
+        this.user = user;
+        this.event = event;
+    }
+
+    // Setters and Getters
+
+    public User getUser() {
+        return user;
+    }
+
+    // You should never call this method
+    /*
+    public void setUser(User user) {
+        this.user = user;
+    }
+     */
+
+    public Event getEvent() {
+        return event;
+    }
+
+    // You should never call this method
+    /*
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+     */
 }
+

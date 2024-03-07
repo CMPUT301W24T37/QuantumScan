17d3b9c20c6ec76026700c28e45f907396db4eda
austin-16 <austin727303178@gmail.com>
2024-03-06 23:22:44 -0700

    update User and Attendee, add UnitTest for Attendee and Organizer

--- app/app/src/main/java/com/example/quantumscan/Attendee.java
+++ app/app/src/main/java/com/example/quantumscan/Attendee.java
@@ -3,30 +3,35 @@ package com.example.quantumscan;
 public class Attendee {
     private final User user;
     private final Event event;
-
-
-
+    private final String userID;
+    private final String userName;
     private Boolean checkIn = false;
 
     // Constructors
-
     public Attendee(User user, Event event) {
         this.user = user;
         this.event = event;
+        this.userID = user.getId();
+        this.userName = user.getName();
     }
-
 
     // Setters and Getters
     public Boolean getCheckIn() {
         return checkIn;
     }
-
     public void setCheckIn(Boolean checkIn) {
         this.checkIn = checkIn;
     }
     public User getUser() {
         return user;
     }
+    public Event getEvent() { return event; }
 
+    public String getUserID() {
+        return userID;
+    }
 
+    public String getUserName() {
+        return userName;
+    }
 }

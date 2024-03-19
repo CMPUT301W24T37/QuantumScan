12fb0184356cfa4382a7364086862ff86503b52d
“David <davidzhang20040709@gmail.com>
2024-03-19 11:22:53 -0600

    update

---
+++ app/app/src/main/java/com/example/quantumscan/AttendeeContentAdapter.java
@@ -0,0 +1,40 @@
+package com.example.quantumscan;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AttendeeContentAdapter extends ArrayAdapter<Attendee> {
+    public AttendeeContentAdapter(Context context, ArrayList<Attendee> users) {
+        super(context, 0, users);
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Attendee attendee = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.attendee_list_content, parent, false);
+        }
+        // Lookup view for data population
+        TextView nameTextView = (TextView) convertView.findViewById(R.id.attendee_name);
+        TextView checkCountTextView = (TextView) convertView.findViewById(R.id.check_in_num);
+        ImageView checkStatusView = (ImageView) convertView.findViewById(R.id.check_in_status);
+        // Populate the data into the template view using the data object
+        nameTextView.setText(attendee.getUserName());
+        checkCountTextView.setText(String.valueOf(attendee.getCheckInCount()));
+        if(attendee.getCheckIn()){
+            checkStatusView.setImageResource(R.drawable.ic_checked);
+        }else {
+            checkStatusView.setImageResource(R.drawable.ic_unchecked);;
+        }
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
--- app/app/src/main/java/com/example/quantumscan/OrganizerViewAttendees.java
+++ app/app/src/main/java/com/example/quantumscan/OrganizerViewAttendees.java
@@ -17,8 +17,9 @@ import java.util.Arrays;
 public class OrganizerViewAttendees extends AppCompatActivity {
 
     ListView eventListView;
-    ArrayAdapter<String> eventAdapter;
-    ArrayList<String> dataList;
+    AttendeeContentAdapter attendeeAdapter;
+    ArrayList<Attendee> dataList;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.organizer_event_attendees_list);
@@ -32,10 +33,18 @@ public class OrganizerViewAttendees extends AppCompatActivity {
         titleView.setText(eventName);
 
         //String []attendees ={"Austin", "ZhiYang", "Wei","David","Karl","Kaining"};
+<<<<<<< Updated upstream
         dataList = new ArrayList<>();
         //dataList.addAll(Arrays.asList(attendees));
         eventAdapter = new ArrayAdapter<>(this, R.layout.attendee_list_content, dataList);
         eventListView.setAdapter(eventAdapter);
+=======
+
+        dataList = new ArrayList<Attendee>();
+        //dataList.addAll(Arrays.asList(attendees));
+        attendeeAdapter = new AttendeeContentAdapter(this, dataList);
+        eventListView.setAdapter(attendeeAdapter);
+>>>>>>> Stashed changes
 
 
 

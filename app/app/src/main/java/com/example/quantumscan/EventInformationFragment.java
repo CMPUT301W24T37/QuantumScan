202ed1962c06c325847a7b70ddede5efc2810d90
Karl Zhong <158379883+karlzhong2@users.noreply.github.com>
2024-03-29 23:24:18 -0600

    123
    123

--- app/app/src/main/java/com/example/quantumscan/EventInformationFragment.java
+++ app/app/src/main/java/com/example/quantumscan/EventInformationFragment.java
@@ -91,8 +91,8 @@ public class EventInformationFragment extends AppCompatActivity {
         // This might involve updating a Firestore collection to add the user to the event.
         //System.out.println("Join event");
         String currentUserId = getCurrentUserId();
-
-        fireStoreBridge.updateAttendeeSignUpHelper(currentUserId, eventId);
+        System.out.println(currentUserId);
+        fireStoreBridge.updateAttendeeSignUpHelper("1658f5315ca1a74e", eventId);
 
         Toast.makeText(this, "You have joined the event!", Toast.LENGTH_SHORT).show();
     }

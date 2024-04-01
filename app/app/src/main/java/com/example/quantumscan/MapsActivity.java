f634be27723d943529c80bd3562dd49da61a5671
austin-16 <austin727303178@gmail.com>
2024-04-01 07:11:47 -0600

    update MapsActivity, MainActivity, Attendee, AndroidManifest.xml and gradle file for Module

--- app/app/src/main/java/com/example/quantumscan/MapsActivity.java
+++ app/app/src/main/java/com/example/quantumscan/MapsActivity.java
@@ -1,9 +1,14 @@
 package com.example.quantumscan;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,6 +17,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.quantumscan.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.GeoPoint;
 
 import java.util.ArrayList;
 
@@ -21,6 +28,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     private String eventID;
     private String eventName;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         eventID = getIntent().getStringExtra("eventID");
         eventName = getIntent().getStringExtra("eventName");
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
     /**
@@ -57,24 +68,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .title("Marker in University of Alberta")
                 .draggable(false)
                 .icon(BitmapDescriptorFactory.defaultMarker(90.0f)));  // this will be the marker for the last checked-in location of attendee
+
+        // retrieve the attendeeList of the event, and read the location of each attendee
 //        Admin admin = new Admin();
 //        admin.retrieveEventAttendeeList(eventID, new Admin.OnEventAttendeeListRetrievedListener() {
 //            @Override
 //            public void onEventAttendeeListRetrieved(ArrayList<Attendee> attendeeList) {
 //                for (Attendee attendee : attendeeList) {
-//                    //LatLng attendeeLatlng = new LatLng( , );
-//                    mMap.addMarker(new MarkerOptions().position(attendeeLatlng).title(attendee.getName()).draggable(false));  // this will be the marker for the last checked-in location of attendee
+//                    if (attendee.isCheckedIn() && attendee.getLocation() != null) {  // only display the locations from checked-in attendees if locations exist
+//                        // get the location
+//                        Double attendeeLatitude = attendee.getLocation().getLatitude();
+//                        Double attendeeLongitude = attendee.getLocation().getLongitude();
+//                        LatLng attendeeLatlng = new LatLng(attendeeLatitude, attendeeLongitude);
+//
+//                        // this will be the marker for the last checked-in location of attendee
+//                        mMap.addMarker(new MarkerOptions()
+//                                .position(attendeeLatlng)
+//                                .title(attendee.getName())
+//                                .draggable(false));
+//                    }
+//
 //                }
 //            }
 //        });
         mMap.moveCamera(CameraUpdateFactory.newLatLng(uofa));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(3));
 
+        /* // How to do with location permission check
+        // Below is the if statement to check we we have permission for user location,
+        // if we do (in the else block) then do something, if we don't then do something else (in if block)
+        // This will be used in the Attendee check in, once the Attendee has checked in, we use this permission checking to decide if we put Geopoint object or null Object in firebase
+        // Below is also an example of adding the user location marker on Google map depends on whether the use has allowed the permission for retrieving location
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //return;
+        }
+        else {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                LatLng myLoc = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(myLoc)
+                                        .title("Austin Meng")
+                                        .draggable(false)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                            }
+                        }
+                    });
+        }
+
+         */
+
+
         /*
         Todo:
         1. add "last checked-in location" in Attendee (which is the subcollection of the Event)
-        2. iterate every attendee's location, get the latlng stats and make them the markers on map
+        2. iterate every attendee's location, get the Latlng (Geopoint) stats and make them the markers on map
         3. done
+        Tips: check something I've written above, there are some of my explanation.
          */
 
     }

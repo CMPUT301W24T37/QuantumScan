b38b7fc115c18150c86ef9cb755a8d8a577078d5
weiz0123 <76544381+weiz0123@users.noreply.github.com>
2024-04-05 01:14:29 -0600

    mile stone update

--- app/app/src/main/java/com/example/quantumscan/OrganizerViewAttendees.java
+++ app/app/src/main/java/com/example/quantumscan/OrganizerViewAttendees.java
@@ -3,16 +3,14 @@ package com.example.quantumscan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class OrganizerViewAttendees extends AppCompatActivity {
 
@@ -20,6 +18,12 @@ public class OrganizerViewAttendees extends AppCompatActivity {
     AttendeeContentAdapter attendeeAdapter;
     ArrayList<AttendeeFireBaseHolder> dataList;
     FireStoreBridge fb;
+    private ProgressBar mileStoneProgressBar;
+    private TextView mileStoneProgressText;
+    private TextView mileStoneStatement;
+    private int mileStoneValue;
+    private ToastManager toast;
+    private String eventName;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +32,13 @@ public class OrganizerViewAttendees extends AppCompatActivity {
         TextView titleView = findViewById(R.id.textView_eventName);
         TextView pressableTextView = findViewById(R.id.inviteText);
         eventListView = findViewById(R.id.attendeeList);
+        mileStoneProgressBar = findViewById(R.id.milestone_bar);
+        mileStoneProgressText = findViewById(R.id.milestoneMessageText);
+        mileStoneStatement = findViewById(R.id.milestoneTextMessage);
+        toast = ToastManager.getInstance();
 
         String eventID = getIntent().getStringExtra("eventID");
-        String eventName = getIntent().getStringExtra("eventName");
+        eventName = getIntent().getStringExtra("eventName");
         titleView.setText(eventName);
         dataList = new ArrayList<AttendeeFireBaseHolder>();
         fb = new FireStoreBridge("EVENT");
@@ -40,8 +48,13 @@ public class OrganizerViewAttendees extends AppCompatActivity {
             public void onCheckedInListener(ArrayList<AttendeeFireBaseHolder> attendeeList) {
                 if (attendeeList.isEmpty() || attendeeList == null){
                     System.out.println("no attendee joined this event");
+                    mileStoneProgressBar.setMax(0);
+                    mileStoneProgressBar.setProgress(0);
+                    mileStoneProgressText.setText("0");
+                    mileStoneStatement.setText("Invite People to your Event!");
+                }else {
+                    refresh(attendeeList);
                 }
-                refresh(attendeeList);
             }
 
         });
@@ -60,23 +73,52 @@ public class OrganizerViewAttendees extends AppCompatActivity {
                 finish();
             }
         });
-
         pressableTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent detailIntent = new Intent(OrganizerViewAttendees.this, OrganizerEventShare.class);
                 detailIntent.putExtra("eventID", eventID);
                 startActivity(detailIntent);
-
             }
         });
-
     }
 
     private void refresh(ArrayList<AttendeeFireBaseHolder> attendeeList){
         dataList = attendeeList;
         attendeeAdapter = new AttendeeContentAdapter(this, dataList);
         eventListView.setAdapter(attendeeAdapter);
+
+        int count = checkedInCount(attendeeList);
+        mileStoneProgressBar.setMax(dataList.size());
+        mileStoneProgressBar.setProgress(count);
+        mileStoneProgressText.setText(count + " / " + dataList.size());
+        if((int)(count/dataList.size()) == 2){
+            mileStoneStatement.setText("Half Way Reached!");
+            toast.showToast(this, "You have reached a new Mile Stone for" + eventName );
+
+        }else if(count == dataList.size()){
+            mileStoneStatement.setText("Next: Half Way Mile Stone...");
+
+
+        }else if ((int)(count/dataList.size()) > 2){
+            mileStoneStatement.setText("Next: Final Mile Stone...");
+
+
+        }else{
+            mileStoneStatement.setText("Final Mile Stone Reached!");
+            toast.showToast(this, "You have reached a new Mile Stone for "+eventName);
+
+        }
+    }
+
+    private int checkedInCount(ArrayList<AttendeeFireBaseHolder> attendeeList){
+        int count = 0;
+        for(int i = 0; i < attendeeList.size(); i++){
+            if(attendeeList.get(i).getCheckInCount() >= 1){
+                count++;
+            }
+        }
+        return count;
     }
 
 }

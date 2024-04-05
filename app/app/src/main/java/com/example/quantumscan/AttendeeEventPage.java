319b9889957f741a78423452960b4a3e6d44e733
zhiyang5 <zhiyang5@ualberta.ca>
2024-04-05 00:06:27 -0600

    EventTime XML

--- app/app/src/main/java/com/example/quantumscan/AttendeeEventPage.java
+++ app/app/src/main/java/com/example/quantumscan/AttendeeEventPage.java
@@ -43,7 +43,6 @@ public class AttendeeEventPage extends AppCompatActivity {
     private Button buttonReturn;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

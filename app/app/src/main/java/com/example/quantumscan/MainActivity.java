74f811ae5a171453787d1c183679e9834e4e81a5
“David <davidzhang20040709@gmail.com>
2024-04-06 00:32:39 -0600

    update createEvent Page

--- app/app/src/main/java/com/example/quantumscan/MainActivity.java
+++ app/app/src/main/java/com/example/quantumscan/MainActivity.java
@@ -45,20 +45,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
         // authentication start
-//        String userID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-//        FireStoreBridge fb = new FireStoreBridge("USER");
-//        fb.retrieveUser(userID, new FireStoreBridge.OnUserRetrievedListener() {
-//            @Override
-//            public void onUserRetrieved(User user, ArrayList<String> attendeeRoles, ArrayList<String> organizerRoles) {
-//                if (user != null && user.getId() != null && user.getId().trim().equals(userID)) {
-//
-//                } else {
-//                    Intent intent = new Intent(MainActivity.this, UserProfileFileActivity.class);
-//                    startActivity(intent);
-//                }
-//            }
-//
-//        });
+        String userID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        FireStoreBridge fb = new FireStoreBridge("USER");
+        fb.retrieveUser(userID, new FireStoreBridge.OnUserRetrievedListener() {
+            @Override
+            public void onUserRetrieved(User user, ArrayList<String> attendeeRoles, ArrayList<String> organizerRoles) {
+                if (user != null && user.getId() != null && user.getId().trim().equals(userID)) {
+
+                } else {
+                    Intent intent = new Intent(MainActivity.this, UserProfileFileActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+        });
         // authentication end
 
         binding.bottomNavigationView.setOnItemReselectedListener(item -> {
